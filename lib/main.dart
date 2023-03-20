a3c7588c29b62ad9de73940a6bbabb8fba50138c
Raman Hrytsuk <raman_hrytsuk@epam.com>
2023-03-20 09:49:21 +0100

    added loading screen provider and also added to app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:insta_riverpod/state/auth/providers/auth_state_provider.dart';
 import 'package:insta_riverpod/state/auth/providers/is_logged_in_provider.dart';
+import 'package:insta_riverpod/state/providers/is_loading_provider.dart';
+import 'package:insta_riverpod/views/components/loading/loading_screen.dart';
 import 'firebase_options.dart';
 
 // import 'dart:developer' as devtools show log;
@@ -36,6 +38,17 @@ class App extends StatelessWidget {
       darkTheme: ThemeData.dark(useMaterial3: true),
       home: Consumer(
         builder: (context, ref, child) {
+          ref.listen<bool>(
+            isLoadingProvider,
+            (previous, next) {
+              if (next) {
+                LoadingScreen.instance().show(context: context);
+              } else {
+                LoadingScreen.instance().hide();
+              }
+            },
+          );
+
           final isLoggedIn = ref.watch(isLoggedInProvider);
           if (isLoggedIn) {
             return const MainView();
